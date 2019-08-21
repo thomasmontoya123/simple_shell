3e78be7d0218966d8035d9effb65c912f7f0dfa8
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-08-21 12:32:52 -0500

    get user input

---
+++ _getline.c
@@ -0,0 +1,49 @@
+
+#include "shell_header.h"
+
+/**
+ * _getline - get the user input
+ *
+ * Return: pointer to the input line
+ */
+
+char *_getline(void)
+{
+	char *input_line;
+	int ascii_character, index, buffer_size;
+
+	index = 0;
+	buffer_size = 100;
+	input_line = malloc(sizeof(char) * 100);
+	if (!input_line)
+	{
+		perror("Malloc fails\n");
+		return (NULL);
+	}
+	while (1)
+	{
+		ascii_character = _getchar();
+		if (ascii_character == EOF)
+			exit(EXIT_FAILURE);
+
+		else if (ascii_character == '\n')
+		{
+			input_line[index] = '\0';
+			return (input_line);
+		}
+		else
+			input_line[index] = ascii_character;
+
+		index++;
+		if (index >= (buffer_size - 1))
+		{
+			buffer_size += 100;
+			input_line = _realloc(input_line, buffer_size, buffer_size);
+			if (!input_line)
+			{
+				perror("Realloc fails\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
