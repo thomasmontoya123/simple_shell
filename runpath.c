1d88afe55176ec149559e8c2cac353a4878550bd
Dario Castaño <dario.castano@aim.com>
2019-08-23 20:04:02 +0000

    Fix betty style

--- runpath.c
+++ runpath.c
@@ -4,9 +4,9 @@
 
 /**
  * runpath - run a command found it the $PATH
- * 
+ *
  * @arguments: argument vector
- * 
+ *
  * Return: status for main loop
  */
 int runpath(char **arguments)
@@ -14,23 +14,24 @@ int runpath(char **arguments)
 	char *found_path;
 	int exec_status;
 
-        found_path = find_path(arguments[0]);
-        if (found_path == NULL)
-        {
-            perror("Not found");
-            return (0);
-        }
-        else
-        {
-            arguments[0] = found_path;
-            exec_status = execve(arguments[0], arguments, NULL);
-                if (exec_status == -1)
-                {
-                    free(found_path);
-                    perror("Exec error");
-                    return (0);
-                }
-            free(found_path);
-            return (0);
-        }
+	found_path = find_path(arguments[0]);
+	if (found_path == NULL)
+	{
+		perror("Not found");
+		return (0);
+	}
+	else
+	{
+		arguments[0] = found_path;
+		exec_status = execve(arguments[0], arguments, NULL);
+		if (exec_status == -1)
+		{
+			free(found_path);
+			perror("Exec error");
+			return (0);
+		}
+		free(found_path);
+		return (0);
+	}
 }
+
