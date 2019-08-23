ae11b96d6babab0b23465679aac8999522ccf2b9
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-08-23 15:21:50 -0500

    help builtin

---
+++ help.c
@@ -0,0 +1,13 @@
+#include "shell_header.h"
+
+/**
+ * help - print shell help
+ *
+ * Return: void
+ */
+
+int help(__attribute__((unused)) char **arguments)
+{
+	write(1, "here goes the shell help\n", 26);
+	return (1);
+}
