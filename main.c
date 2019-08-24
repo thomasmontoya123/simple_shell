44b3fc83171be54a61a21c360cff7bc149c284b3
Dario Castaño <dario.castano@aim.com>
2019-08-24 18:48:05 -0500

    Disable temporary the _getline() function

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[], char *envp[])
 		if (isatty(STDIN_FILENO))
 			write(1, "($) ", 4);
 
-		input_line = _getline();
+		input_line = line_reader();
 		arguments = line_spliter(input_line);
 		status = launcher(arguments, envp);
 		free(input_line);
