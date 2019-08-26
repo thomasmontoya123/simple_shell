1885921dbabeac3f67daeca99c819bdcb5647b99
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-08-26 17:27:53 -0500

    _getline inplmented

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[], char *envp[])
 		if (isatty(STDIN_FILENO))
 			write(1, "($) ", 4);
 
-		input_line = line_reader();
+		input_line = _getline();
 		arguments = line_spliter(input_line);
 		status = launcher(arguments, envp);
 		free(input_line);
