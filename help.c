b9ec1a5c7091c22d447abfa355fea30d6a81b584
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-08-27 17:13:55 -0500

    help for each builtin

--- help.c
+++ help.c
@@ -9,6 +9,29 @@
 
 int help(__attribute__((unused)) char **arguments)
 {
-	write(1, "here goes the shell help\n", 26);
+	if (!arguments[1])
+	{
+		write(1, "help without args\n", 19);
+		return (1);
+	}
+
+	else if (_strcmp(arguments[1], "cd") == 0)
+	{
+		write(1, "help cd\n", 9);
+		return (1);
+	}
+
+	else if (_strcmp(arguments[1], "env") == 0)
+	{
+		write(1, "help env\n", 10);
+		return (1);
+	}
+
+	else if (_strcmp(arguments[1], "exit") == 0)
+	{
+		write(1, "help exit\n", 11);
+		return (1);
+	}
+
 	return (1);
 }
